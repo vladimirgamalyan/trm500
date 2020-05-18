131b543725f930b427c84bc5fd4ea5967bb3528c
Vladimir Gamalyan <vladimir.gamalyan@gmail.com>
2020-05-18 20:41:24 +0300

    Add device data

--- devices.js
+++ devices.js
@@ -6,20 +6,24 @@ Vue.component('devices', {
         <div v-for="item in privateState.data" :key="item.id">
             {{ item.type }}
         </div>
+        <div>
+            <pre>{{ privateState.deviceData  }}</pre>
+        </div>
      </div>`,
 
     data: function () {
         return {
             privateState: {
-                data: []
+                data: [],
+                deviceData: {foo: 42}
             },
             sharedState: store
         }
     },
 
     methods: {
-        loadData: function () {
-            console.log('load data');
+        loadDevices: function () {
+            console.log('load devices');
             let vm = this;
 
             axios
@@ -40,6 +44,38 @@ Vue.component('devices', {
                     vm.privateState.data = response.data;
                     console.log(response.data);
                     //vm.sharedState.authorized = true;
+
+                    vm.loadDevice(response.data[0].id);
+                })
+                .catch(function (error) {
+                    if (error.response) {
+                        console.log(error.response.data);
+                        console.log(error.response.status);
+                        //vm.privateState.message = error.response.data.message;
+                    }
+                })
+        },
+
+        loadDevice: function (id) {
+            console.log('load device');
+            let vm = this;
+
+            axios
+                .request({
+                    method: 'POST',
+                    headers: {
+                        'accept': '*/*',
+                        'Authorization': 'Bearer ' + vm.sharedState.token,
+                        'content-type': 'application/x-www-form-urlencoded',
+
+                    },
+                    url: '/v1/device/' + id,
+                    data: []
+                })
+                .then(function (response) {
+                    vm.privateState.deviceData = response.data;
+                    console.log(response.data);
+                    //vm.sharedState.authorized = true;
                 })
                 .catch(function (error) {
                     if (error.response) {
@@ -53,11 +89,11 @@ Vue.component('devices', {
 
     created: function () {
         console.log('devices ready');
-        this.loadData();
+        this.loadDevices();
 
-        setInterval(function () {
-            this.loadData();
-        }.bind(this), 5000);
+        // setInterval(function () {
+        //     this.loadData();
+        // }.bind(this), 5000);
     },
     beforeDestroy() {
         if (this.interval) {
