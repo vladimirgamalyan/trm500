8cbca8720c3337e04c8a1753a6f9153157b523c6
Vladimir Gamalyan <vladimir.gamalyan@gmail.com>
2020-05-18 21:45:56 +0300

    Add temperature control

--- devices.js
+++ devices.js
@@ -6,9 +6,13 @@ Vue.component('devices', {
         <div v-for="item in privateState.data" :key="item.id">
             {{ item.type }}
         </div>
+        
+        <button v-on:click="setTemp(20)">20</button>
+        <button v-on:click="setTemp(30)">30</button>
+        <button v-on:click="loadDevices">Refresh</button>
                 
         <div v-for="item in privateState.deviceData.parameters" :key="item.id">
-            <pre>{{ item.name }} {{ item.value }}</pre>
+            <pre>{{ item.id }}  {{ item.name }} {{ item.value }}</pre>
         </div>
         
         <div>
@@ -27,6 +31,45 @@ Vue.component('devices', {
     },
 
     methods: {
+        setTemp: function (temp) {
+            console.log('set temp', temp);
+            let vm = this;
+
+            axios
+                .request({
+                    method: 'POST',
+                    headers: {
+                        'accept': '*/*',
+                        'Authorization': 'Bearer ' + vm.sharedState.token,
+                        'content-type': 'application/x-www-form-urlencoded',
+                    },
+                    url: '/v1/parameters/write-data',
+                    data: {
+                        "timeout":60,
+                        "sync":true,
+                        "data":[
+                            {
+                                "id": 4369491,
+                                "value": temp.toString()
+                            }
+                        ]
+                    }
+                })
+                .then(function (response) {
+                    //vm.privateState.data = response.data;
+                    console.log(response.data);
+                    //vm.sharedState.authorized = true;
+
+                    vm.loadDevices();
+                })
+                .catch(function (error) {
+                    if (error.response) {
+                        console.log(error.response.data);
+                        console.log(error.response.status);
+                        //vm.privateState.message = error.response.data.message;
+                    }
+                })
+        },
         loadDevices: function () {
             console.log('load devices');
             let vm = this;
@@ -38,7 +81,6 @@ Vue.component('devices', {
                         'accept': '*/*',
                         'Authorization': 'Bearer ' + vm.sharedState.token,
                         'content-type': 'application/x-www-form-urlencoded',
-
                     },
                     url: '/v1/device/index',
                     data: {
@@ -72,7 +114,6 @@ Vue.component('devices', {
                         'accept': '*/*',
                         'Authorization': 'Bearer ' + vm.sharedState.token,
                         'content-type': 'application/x-www-form-urlencoded',
-
                     },
                     url: '/v1/device/' + id,
                     data: []
