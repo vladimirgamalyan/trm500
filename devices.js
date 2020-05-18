db7acb076f47293638185f156ee3d281fe519fa7
Vladimir Gamalyan <vladimir.gamalyan@gmail.com>
2020-05-18 21:54:08 +0300

    Add discrete output control

--- devices.js
+++ devices.js
@@ -9,6 +9,8 @@ Vue.component('devices', {
         
         <button v-on:click="setTemp(20)">20</button>
         <button v-on:click="setTemp(30)">30</button>
+        <button v-on:click="setDiscrete(0)">OFF</button>
+        <button v-on:click="setDiscrete(1)">ON</button>
         <button v-on:click="loadDevices">Refresh</button>
                 
         <div v-for="item in privateState.deviceData.parameters" :key="item.id">
@@ -31,6 +33,45 @@ Vue.component('devices', {
     },
 
     methods: {
+        setDiscrete: function (value) {
+            console.log('set setDiscrete', value);
+            let vm = this;
+
+            axios
+                .request({
+                    method: 'POST',
+                    headers: {
+                        'accept': '*/*',
+                        'Authorization': 'Bearer ' + vm.sharedState.token,
+                        'content-type': 'application/x-www-form-urlencoded',
+                    },
+                    url: '/v1/parameters/write-data',
+                    data: {
+                        "timeout":60,
+                        "sync":true,
+                        "data":[
+                            {
+                                "id": 4369461,
+                                "value": value.toString()
+                            }
+                        ]
+                    }
+                })
+                .then(function (response) {
+                    //vm.privateState.data = response.data;
+                    console.log(response.data);
+                    //vm.sharedState.authorized = true;
+
+                    vm.loadDevices();
+                })
+                .catch(function (error) {
+                    if (error.response) {
+                        console.log(error.response.data);
+                        console.log(error.response.status);
+                        //vm.privateState.message = error.response.data.message;
+                    }
+                })
+        },
         setTemp: function (temp) {
             console.log('set temp', temp);
             let vm = this;
